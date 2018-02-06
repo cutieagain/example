54ec89d62d0943deb6c325047a53a43ed10e2524
330449857@qq.com <chenyang>
2018-02-06 23:56:45 +0800

    cutie 1.thread_pool_executor

--- src/main/java/com/cutie/java_grammar/concurrency/thread_pool_executor/package-info.java
+++ src/main/java/com/cutie/java_grammar/concurrency/thread_pool_executor/package-info.java
@@ -3,7 +3,11 @@
  */
 package com.cutie.java_grammar.concurrency.thread_pool_executor;
 /*
+  article 1:ThreadPoolExecutor使用和思考(上)-线程池大小设置与BlockingQueue的三种实现区别
+*   http://dongxuan.iteye.com/blog/901689
 *
+* article 2:深入理解java线程池—ThreadPoolExecutor (源码理解)
+*   https://www.jianshu.com/p/ade771d2c9c0
 *
 *
 *
