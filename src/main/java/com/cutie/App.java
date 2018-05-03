3db6e9d43f1447ddd0ed051a20d3dfb6cf114cf9
cutieagain <330449857@qq.com>
2018-05-03 14:42:41 +0800

    陈奕扬 1、forgot

--- src/main/java/com/cutie/App.java
+++ src/main/java/com/cutie/App.java
@@ -9,10 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        String s = "帅o1\u001cN(帅-_";
-//        String a = s.replaceAll("[^0-9a-zA-Z-_]", "");
-        String a = s.replaceAll("[^\\w-]", "");
-
-        System.out.println(a);
     }
 }
